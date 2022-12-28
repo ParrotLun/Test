a5264c3c50d1dc1f203e3223b869b5ab76898a41
ParrotLun <118044995+ParrotLun@users.noreply.github.com>
2022-12-28 21:16:27 +0800

    gg

---
+++ ConsoleApp1Tests/MupTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Tests
+{
+    [TestClass()]
+    public class MupTests
+    {
+        [TestMethod()]
+        public void mutiplTest()
+        {
+            string[] args = { "8", "7" };
+            int result = ConsoleApp.Mup.mutipl(args);
+
+            Assert.AreEqual(result,56);
+        }
+        [TestMethod()]
+        public void GG()
+        {
+            string[] args = { "9", "7" };
+            int result = ConsoleApp.Mup.mutipl(args);
+
+            Assert.AreEqual(result, 63);
+        }
+    }
+}
